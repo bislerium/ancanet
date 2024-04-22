4f15163a4b316e613e2bc536e1fbd75af6f6dbdc
Bishal <chhetrig.bishal@gmail.com>
2024-04-22 13:52:53 +0545

    Life is hard

---
+++ src/ancanet.server/Managers/ProfileManager.cs
@@ -0,0 +1,12 @@
+﻿using ancanet.server.Data;
+
+namespace ancanet.server.Managers
+{
+    public class ProfileManager(AppDbContext appDbContext)
+    {
+        public Task AddProfile()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
